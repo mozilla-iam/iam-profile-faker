c9008f5aff5288d7fa732ecb2a9dbd2b66ba555c
Tasos Katsoulas <akatsoulas@gmail.com>
2018-07-16 20:36:57 +0300

    Expose endpoints for v2 schema compatible profiles.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', 'Faker']
+requirements = ['Click>=6.0', 'Faker', 'flata']
 
 setup_requirements = []
 
