a73ab898b2d396799568d3642494f639a618c56e
John Giannelos <johngiannelos@gmail.com>
2018-07-18 17:04:19 +0200

    Multiple fixes to generate a valid JSON
    * Calculate 'value' and 'values' key based on type
    * Add signature/metadata values
    * Fix access_information section structure
    * Change `schema` key name

--- iam_profile_faker/factory.py
+++ iam_profile_faker/factory.py
@@ -4,6 +4,30 @@ import random
 from faker import Faker
 
 
+def wrap_metadata_signature(obj, value):
+    """Wrap profile value with metadata/signature"""
+
+    # Value key varies based on the type of the value
+    if isinstance(value, dict) or isinstance(value, list):
+        value_key = 'values'
+    else:
+        value_key = 'value'
+
+    return {
+        value_key: value,
+        'metadata': obj.metadata(),
+        'signature': obj.signature()
+    }
+
+
+def decorate_metadata_signature(fun):
+    """Decorate faker classes to wrap results with metadata/signature."""
+    def wrapper(*args, **kwargs):
+        value = fun(*args, **kwargs)
+        return wrap_metadata_signature(args[0], value)
+    return wrapper
+
+
 class IAMFaker(object):
     def __init__(self, locale=None):
         self.fake = Faker(locale)
@@ -12,6 +36,45 @@ class IAMFaker(object):
         """Profile v2 schema faker."""
         return 'https://person-api.sso.mozilla.com/schema/v2/profile'
 
+    def metadata(self):
+        """Generate field metadata"""
+
+        classifications = [
+            'MOZILLA CONFIDENTIAL',
+            'WORKGROUP CONFIDENTIAL: STAFF ONLY',
+            'PUBLIC',
+            'INDIVIDUAL CONFIDENTIAL'
+        ]
+        publisher_authority = [
+            'access_provider', 'ldap', 'hris', 'cis', 'mozilliansorg'
+        ]
+        created = self.fake.date_time()
+        last_modified = self.fake.date_time_between_dates(datetime_start=created)
+
+        return {
+            'classification': random.choice(classifications),
+            'last_modified': last_modified.isoformat(),
+            'created': created.isoformat(),
+            'publisher_authority': random.choice(publisher_authority),
+            'verified': self.fake.pybool()
+        }
+
+    def signature(self):
+        """Generate field signature"""
+
+        def _gen_signature():
+            return {
+                'alg': 'RS256',
+                'typ': 'JWT',
+                'value': '{}.{}.{}'.format(self.fake.pystr(), self.fake.pystr(), self.fake.pystr())
+            }
+
+        return {
+            'publisher': _gen_signature(),
+            'additional': [_gen_signature() for i in range(random.randint(0, 5))]
+        }
+
+    @decorate_metadata_signature
     def login_method(self):
         """Profile v2 login_method faker."""
         login_methods = [
@@ -19,6 +82,7 @@ class IAMFaker(object):
         ]
         return random.choice(login_methods)
 
+    @decorate_metadata_signature
     def user_id(self, login_method=None):
         """Profile v2 user_id attribute faker."""
         user_ids = [
@@ -31,11 +95,12 @@ class IAMFaker(object):
 
         if login_method:
             for uid in user_ids:
-                if uid.startswith(login_method):
+                if uid.startswith(login_method['value']):
                     return uid
 
         return random.choice(user_ids)
 
+    @decorate_metadata_signature
     def usernames(self):
         """Profile v2 usernames faker."""
         values = {}
@@ -44,6 +109,7 @@ class IAMFaker(object):
 
         return values
 
+    @decorate_metadata_signature
     def identities(self):
         """Profile v2 identities faker."""
         values = {}
@@ -52,6 +118,7 @@ class IAMFaker(object):
 
         return values
 
+    @decorate_metadata_signature
     def ssh_public_keys(self):
         """Profile v2 public SSH key faker."""
         values = {}
@@ -62,6 +129,7 @@ class IAMFaker(object):
 
         return values
 
+    @decorate_metadata_signature
     def pgp_public_keys(self):
         """Profile v2 public PGP key faker."""
         values = {}
@@ -76,13 +144,16 @@ class IAMFaker(object):
     def access_level(self):
         """Profile v2 access level faker."""
         values = {}
-        for publisher in ['ldap', 'mozilliansorg']:
-            values[publisher] = {}
-            for _ in range(random.randint(0, 5)):
-                values[publisher][self.fake.slug()] = self.fake.pybool()
+        for publisher in ['ldap', 'mozilliansorg', 'hris', 'access_provider']:
+            v = {}
+            for _ in range(random.randint(1, 5)):
+                v[self.fake.slug()] = self.fake.pybool()
+
+            values[publisher] = wrap_metadata_signature(self, v)
 
         return values
 
+    @decorate_metadata_signature
     def office_location(self):
         """Profile v2 office location faker."""
         locations = [
@@ -92,6 +163,7 @@ class IAMFaker(object):
 
         return random.choice(locations)
 
+    @decorate_metadata_signature
     def preferred_languages(self):
         """Profile v2 preferred languages faker."""
         values = []
@@ -100,10 +172,12 @@ class IAMFaker(object):
 
         return values
 
+    @decorate_metadata_signature
     def pronouns(self):
         """Profile v2 pronouns faker."""
         return random.choice([None, 'he/him', 'she/her', 'they/them'])
 
+    @decorate_metadata_signature
     def uris(self):
         """Profile v2 URIs faker."""
         values = {}
@@ -112,6 +186,7 @@ class IAMFaker(object):
 
         return values
 
+    @decorate_metadata_signature
     def phone_numbers(self):
         """Profile v2 phone_numbers faker."""
         values = {}
@@ -127,32 +202,32 @@ class IAMFaker(object):
         last_modified = self.fake.date_time_between_dates(datetime_start=created)
 
         obj = {
-            '$schema': self.schema(),
+            'schema': self.schema(),
             'login_method': login_method,
             'user_id': self.user_id(login_method=login_method),
-            'active': self.fake.pybool(),
-            'last_modified': last_modified.isoformat(),
-            'created': created.isoformat(),
+            'active': wrap_metadata_signature(self, self.fake.pybool()),
+            'last_modified': wrap_metadata_signature(self, last_modified.isoformat()),
+            'created': wrap_metadata_signature(self, created.isoformat()),
             'usernames': self.usernames(),
-            'first_name': self.fake.first_name(),
-            'last_name': self.fake.last_name(),
-            'primary_email': self.fake.email(),
+            'first_name': wrap_metadata_signature(self, self.fake.first_name()),
+            'last_name': wrap_metadata_signature(self, self.fake.last_name()),
+            'primary_email': wrap_metadata_signature(self, self.fake.email()),
             'identities': self.identities(),
             'ssh_public_keys': self.ssh_public_keys(),
             'pgp_public_keys': self.pgp_public_keys(),
             'access_information': self.access_level(),
-            'fun_title': self.fake.sentence(),
-            'description': self.fake.paragraph(),
-            'location_preference': self.fake.country(),
+            'fun_title': wrap_metadata_signature(self, self.fake.sentence()),
+            'description': wrap_metadata_signature(self, self.fake.paragraph()),
+            'location_preference': wrap_metadata_signature(self, self.fake.country()),
             'office_location': self.office_location(),
-            'timezone': self.fake.timezone(),
+            'timezone': wrap_metadata_signature(self, self.fake.timezone()),
             'preferred_languages': self.preferred_languages(),
-            'tags': self.fake.words(),
+            'tags': wrap_metadata_signature(self, self.fake.words()),
             'pronouns': self.pronouns(),
-            'picture': self.fake.image_url(),
+            'picture': wrap_metadata_signature(self, self.fake.image_url()),
             'uris': self.uris(),
             'phone_numbers': self.phone_numbers(),
-            'alternative_name': self.fake.name()
+            'alternative_name': wrap_metadata_signature(self, self.fake.name())
         }
 
         return obj
