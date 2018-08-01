8fc727591f7019a67d498c0e8b09fcd034f4ac86
Tasos Katsoulas <akatsoulas@gmail.com>
2018-08-01 13:03:14 +0300

    Expose a GraphQL url in Flask.

--- iam_profile_faker/v2_api.py
+++ iam_profile_faker/v2_api.py
@@ -2,36 +2,40 @@ import os
 from tinydb import TinyDB
 
 from flask import Flask
+from flask_graphql import GraphQLView
 from flask_restful import Resource, Api
+from graphene import Schema
 
 from iam_profile_faker import V2ProfileFactory
+from iam_profile_faker.schema import Query
 
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class RandomUsers(Resource):
-    """Return users from the profile faker."""
-
-    def get(self, count=100):
-        factory = V2ProfileFactory()
-        return factory.create_batch(count)
-
-
-class RandomUser(Resource):
-    """Return a single user."""
-
-    def get(self):
-        return V2ProfileFactory().create()
-
-
+# Helper functions
 def _load_db():
     """Load the saved db file."""
     path = os.path.dirname(os.path.abspath(__file__))
     for file in os.listdir(path):
         if file.endswith('.json'):
             return os.path.join(path, file)
+
+
+class RandomUsers(Resource):
+    """Return users from the profile faker."""
+
+    def get(self, count=100, export_json=True):
+        factory = V2ProfileFactory()
+        return factory.create_batch(count, export_json=True)
+
+
+class RandomUser(Resource):
+    """Return a single user."""
+
+    def get(self, export_json=True):
+        return V2ProfileFactory().create(export_json=export_json)
 
 
 class PersistentUsers(Resource):
@@ -52,10 +56,18 @@ class PersistentUser(Resource):
         return db.all()[user_id]
 
 
+view_func = GraphQLView.as_view(
+    'graphql',
+    schema=Schema(query=Query),
+    graphiql=True
+)
+
+
 api.add_resource(RandomUsers, '/', '/users')
 api.add_resource(RandomUser, '/user')
 api.add_resource(PersistentUsers, '/persistent/users')
 api.add_resource(PersistentUser, '/persistent/user/<int:user_id>')
+app.add_url_rule('/graphql', view_func=view_func)
 
 
 def main():
